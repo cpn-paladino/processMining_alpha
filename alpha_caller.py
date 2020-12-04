478a17d4fe2a22add92216dfe11945c4476e2bd5
Christopher Paladino <cpn.paladino@gmail.com>
2020-12-04 12:55:28 -0300

    added example and requirements

--- alpha_caller.py
+++ alpha_caller.py
@@ -3,11 +3,7 @@ import sys
 sys.path.append('alpha_project')
 # import main dependency
 from alpha_project.mainTest import execute_script
-# pass file path
-log_ruido = r'logs_input/ruido.xes'
-def call(file):
-    #execute_script(file)
-    execute_script(log_ruido)
 
-#call(log_ruido)   
-
+# main function to call alpha miner process
+def call(file):    
+    execute_script(file)
---
+++ example_local.py
@@ -0,0 +1,7 @@
+from alpha_caller import call
+
+''' example of call to local running '''
+# path of XES file
+log_ruido = r'logs_input/ruido.xes'
+# main function
+call(log_ruido)   
